e2982a1b3c7dcde36701c3cb1390634b9fe51b6f
ccostoso <carlos.costoso@outlook.com>
2020-01-13 19:30:22 -0500

    Finished app and README, now to do animated GIFs

--- README.md
+++ README.md
@@ -4,11 +4,28 @@ LIRIbot
 # Introduction
 LIRIbot is a Language Interpretation and Recognition Interface that can provide users with information about any given music band, such as upcoming performances in the user's area.
 
-Clearly state the problem the app is trying to solve (i.e. what is it doing and why)
-Give a high-level overview of how the app is organized
-Give start-to-finish instructions on how to run the app
-Include screenshots, gifs or videos of the app functioning
-Contain a link to a deployed version of the app
-Clearly list the technologies used in the app
-State your role in the app development
+# Commands
 
+* LIRIbot takes in a variety of commands, which, when followed by a parameter, can provide you with information about live performances, songs and movies!
+
+* Here's a list of available commands:
+
+    * node liri concert-this [$PARAM]
+        
+        * "concert-this" can give you a list of three upcoming concerts being held by an artist. Simply replace [$PARAM] is an the name of an artist you'd like to see in-person!
+
+    * node liri spotify-this-song [$PARAM]
+
+        * "spotify-this-sing" can give you information about a song on Spotify, including a preview link! As above, replace [$PARAM] with the name of a song to pull up the information.
+
+    * node liri movie-this [$PARAM]
+
+        * "movie-this" will provide you with information about a movie listed on the Open Movie Database. Simply replace [$PARAM] with the name of a movie to retrieve the information!
+
+    * node liri do-what-it-says
+
+        * You can edit random.txt to input one of the above commands, and comma and parameter to get LIRI to execute said function!
+
+LIRIbot uses Javascript running in a Node.js environment to pull information from Spotify, OMDB and Bands in Town APIs. In addition, LIRIbot employs the Axios, Spotify, Moment and DotEnv dependencies.
+
+If you'd like to see a history of your history, check out the "log.txt" file in LIRIbot's root directory.
--- liri.js
+++ liri.js
@@ -4,63 +4,90 @@ let Spotify = require('node-spotify-api');
 let spotify = new Spotify(keys.spotify);
 const axios = require("axios");
 
+const fs = require("fs");
+
 const input = process.argv.slice(2);
 const command = input[0]
-const parameter = input.slice(1).join(" ");
+let parameter = input.slice(1).join(" ");
 
-switch (command) {
-    case "concert-this":
-        const url = "https://rest.bandsintown.com/artists/" + parameter + "/events?app_id=codingbootcamp";
-        
-        axios
-            .get(url)
-            .then(response => {
-                // console.log(response.data);
-                console.log(`Here are three upcoming events for ${parameter}:`);
-
-                const top3 = response.data.slice(0,3);
-
-                top3.forEach((ele, i) => {
-                    console.log(`${i + 1}:`);
-                    console.log(`Venue name: ${ele.venue.name}`);
-                    if (ele.venue.region !== "" ) {
-                        console.log(`Venue location: ${ele.venue.city},  ${ele.venue.region}`);
-                    } else {
-                        console.log(`Venue location: ${ele.venue.city}`);
-                    }
-                    console.log(`${ele.datetime}`);
+const app = (com, para) => {
+    switch (com) {
+        case "concert-this":
+            const url = "https://rest.bandsintown.com/artists/" + para + "/events?app_id=codingbootcamp";
+            
+            axios
+                .get(url)
+                .then(response => {
+                    console.log(`Here are three upcoming events for ${para}:`);
+    
+                    const top3 = response.data.slice(0,3);
+    
+                    top3.forEach((ele, i) => {
+                        console.log(`${i + 1}:`);
+                        console.log(`Venue name: ${ele.venue.name}`);
+                        if (ele.venue.region !== "" ) {
+                            console.log(`Venue location: ${ele.venue.city},  ${ele.venue.region}`);
+                        } else {
+                            console.log(`Venue location: ${ele.venue.city}`);
+                        }
+                        console.log(`${ele.datetime}`);
+                    })
                 })
-            })
-            .catch(err => {
-                console.log(err);
+                .catch(err => {
+                    console.log(err);
+                });
+            break;
+        case "spotify-this-song":       
+            if (!para) para = "The Sign";
+        
+            spotify
+                .search({type: "track", query: para})
+                .then(response => {
+                    const top = response.tracks.items[0];
+    
+                    console.log(
+                        `Artist: ${top.artists[0].name}
+                        \nName: ${top.name}
+                        \nLink on Spotify: ${top.album.external_urls.spotify}
+                        \nAlbum Name: ${top.album.name}`
+                    );
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+            break;
+        case "movie-this":
+            if (!para) para = "Mr. Nobody";
+    
+            axios.get(`http://www.omdbapi.com/?t=${para}&y=&plot=short&apikey=trilogy`)
+            .then( response => {
+                  console.log("The movie's rating is: " + response.data.imdbRating);
+                  console.log(`Title: ${response.data["Title"]}
+                  \nYear: ${response.data["Year"]}
+                  \nIMDB Rating: ${response.data["imdbRating"]}
+                  \nRotten Tomatoes Rating: ${response.data["Ratings"][1]["Value"]}
+                  \nCountry: ${response.data["Country"]}
+                  \nLanguage: ${response.data["Language"]}
+                  \nPlot: ${response.data["Plot"]}
+                  \nActors: ${JSON.stringify(response.data["Actors"])}`);
+                }
+              );
+            break;
+        case "do-what-it-says":
+            fs.readFile("./random.txt", "utf8", (err, data) => {
+                if (err) throw err;
+                let instructions = data.split(",");
+                app(instructions[0], instructions[1]);
             });
-        break;
-    case "spotify-this-song":
-        console.log("looking good mate");
-    
-        if (!parameter) parameter = "All the Small Things";
-    
-        spotify
-            .search({type: "track", query: parameter})
-            .then(response => {
-                console.log(JSON.stringify(response));
-
-
-            })
-            .catch(err => {
-                console.log(err);
-            })
-        break;
-    case "movie-this":
-
-        break;
-    case "do-what-it-says":
-
-        break;
-    default:
-        console.log("Try again!");
+            break;
+        default:
+            console.log("Try again!");
+    }
 }
 
-
-
-// Date of the Event (use moment to format this as "MM/DD/YYYY")
+app(command, parameter);
+const log = `${command}, ${parameter}\n`;
+fs.appendFile("log.txt", log, "utf8", err => {
+    if (err) throw err;
+    console.log("log appended!");
+})
